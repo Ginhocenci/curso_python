8b153d61c8e0d0720eb9b2224721ba776012754a
Ginhocenci <63825034+Ginhocenci@users.noreply.github.com>
2025-10-10 12:04:36 -0300

    Update aula29_try_except.py

--- aula29_try_except.py
+++ aula29_try_except.py
@@ -12,7 +12,6 @@ try:
 except:
     print('Isso não é um número')
 
-
 # if numero_str.isdigit():
 #     numero_float = float(numero_str)
 #     print(f'O dobro de {numero_str} é {numero_float * 2}')
