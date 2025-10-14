4a122aee2b408a06da3896ff4290a8c446dfc71e
Ginhocenci <63825034+Ginhocenci@users.noreply.github.com>
2025-10-14 17:43:57 -0300

    Create aula44_for_range.py

---
+++ aula44_for_range.py
@@ -0,0 +1,10 @@
+"""
+For + Range
+range -> range(start, stop, step)
+"""
+# numeros = range(0, 100, 8)
+# numeros = range(10, 0, -1)
+numeros = range(0, 10)
+
+for numero in numeros:
+    print(numero)
