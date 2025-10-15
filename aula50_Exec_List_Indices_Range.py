0a9ab48f658d34978ca7deeac26f8c803a3383d1
Ginhocenci <63825034+Ginhocenci@users.noreply.github.com>
2025-10-15 14:38:08 -0300

    Create aula50_Exec_List_Indices_Range.py

---
+++ aula50_Exec_List_Indices_Range.py
@@ -0,0 +1,14 @@
+"""
+Exercício
+Exiba os índices da lista
+Maria
+Helena
+Luiz
+"""
+lista = ['Maria', 'Helena', 'Luiz']
+lista.append('João')
+
+indices = range(len(lista))
+
+for indice in indices:
+    print(indice, lista[indice], type(lista[indice]))
