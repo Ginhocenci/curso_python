62c9e6a5b75ca8e19e6769bbe0d53b090d2c2370
Ginhocenci <63825034+Ginhocenci@users.noreply.github.com>
2025-10-15 14:37:38 -0300

    Create aula48_list.py

---
+++ aula48_list.py
@@ -0,0 +1,19 @@
+"""
+Listas em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - indices e fatiamento
+Métodos úteis: append, insert, pop, del, clear, extend, +
+"""
+#        +01234
+#        -54321
+String = 'ABCDE' # caracteres
+# print(bool([])) #falsy
+# print(lista, type(lista))
+#.........0.....1........2..........3...4
+#........-5....-4.......-3.........-2..-1
+lista = [123, True, 'Luiz Otávio', 1.2, []]
+# print(lista[2].upper(), type(lista[2]))
+lista[-3] = 'Maria'
+print(lista[2], type(lista[2]))
+print(lista)
