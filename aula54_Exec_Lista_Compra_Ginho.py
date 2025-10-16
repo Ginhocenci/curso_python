3971cfe8796fc308dad123b66f66776cad3585f0
Ginhocenci <63825034+Ginhocenci@users.noreply.github.com>
2025-10-16 17:08:03 -0300

    Create aula54_Exec_Lista_Compra_Ginho.py

---
+++ aula54_Exec_Lista_Compra_Ginho.py
@@ -0,0 +1,42 @@
+"""
+Faça uma lista de comprar com listas
+O usuário deve ter a possibilidade de
+inserir, apagar e listar valores da sua lista
+Não permita que o programa quebre com
+erros de índices inexistentes na lista.
+"""
+import os
+lista = []
+indice = 0
+
+while True:
+    print('Selecione uma opção')
+    opcao = input('[i]nserir [a]pagar [l]istar: ')
+    if len(opcao) > 1:
+        print('Digite apenas letra.')
+        continue
+
+    if opcao == 'i':
+        lista.append(input('Valor: '))
+        os.system('cls')
+    elif opcao == 'a':
+        indice_apagar = input('Escolha o índice para apagar:')
+        if not indice_apagar.isdecimal():
+            print('O indice precisa ser um número inteiro.')
+            continue
+        
+        indice_apagar_int = int(indice_apagar)
+        
+        if indice_apagar_int >= len(lista):
+            print('O índice não existe.')
+            continue
+        
+        lista.pop(indice_apagar_int)
+        os.system('cls')
+        
+    elif opcao == 'l':
+        os.system('cls')
+        for indice, nome in enumerate(lista):
+            print(indice, nome)
+    else:
+        print('Opção não encontrada.')
