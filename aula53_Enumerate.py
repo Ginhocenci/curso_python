09eded0fca6b7b1b707f54b4a3deeffc31f0ee1b
Ginhocenci <63825034+Ginhocenci@users.noreply.github.com>
2025-10-16 17:08:00 -0300

    Create aula53_Enumerate.py

---
+++ aula53_Enumerate.py
@@ -0,0 +1,26 @@
+"""
+enumerate - enumera iteráveis (índices)
+"""
+lista = ['Maria', 'Helena', 'Luiz']
+lista.append('João')
+
+# lista_enumerada = enumerate(lista)
+# # print(lista_enumerada)
+
+# for item in lista_enumerada:
+#     print(item)
+
+# lista_enumerada = list(enumerate(lista, start=10))
+# print(lista_enumerada)
+
+# for item in enumerate(lista):
+#     indice, nome = item
+#     print(indice, nome)
+
+for indice, nome in enumerate(lista):
+    print(indice, nome)
+
+# for tupla_enumerada in enumerate(lista):
+#     print('FOR da tupla')
+#     for valor in tupla_enumerada:
+#         print(f'\t{valor}')
