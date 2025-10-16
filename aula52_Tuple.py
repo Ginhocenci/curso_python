ccbcc6dd9fb254599eb4357d4527192a1bf98adc
Ginhocenci <63825034+Ginhocenci@users.noreply.github.com>
2025-10-16 17:07:58 -0300

    Create aula52_Tuple.py

---
+++ aula52_Tuple.py
@@ -0,0 +1,12 @@
+"""
+Tipo tupla - Uma lista imutável
+"""
+nome = 'Maria', 'Helena', 'Luiz'
+nome = ('Maria', 'Helena', 'Luiz')
+# nome = ['Maria', 'Helena', 'Luiz']
+# nomes = tuple(nome)
+# nome = list(nomes)
+# nome[1] = 'Outro' # TypeError: 'tuple' object does not support item assignment
+print(nome[0])
+print(nome)
+# print(nomes)
