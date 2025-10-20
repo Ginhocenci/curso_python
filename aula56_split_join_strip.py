c10559ce0de9f135ac53b797bf7e32f9aefcaae0
Ginhocenci <63825034+Ginhocenci@users.noreply.github.com>
2025-10-20 17:32:03 -0300

    Update aula56_split_join_strip.py

--- aula56_split_join_strip.py
+++ aula56_split_join_strip.py
@@ -8,9 +8,9 @@ lista_frases_cruas = frase.split(',')
 
 lista_frases = []
 for i, frase in enumerate(lista_frases_cruas):
-    lista_frases.append(lista_frases_cruas[i].strip())
+    lista_frases.append(lista_frases_cruas[i].strip()) # rstrip e lstrip contar dos lados esq/dir
 
-print(lista_frases_cruas)
-print(lista_frases)
+# print(lista_frases_cruas)
+# print(lista_frases)
 frases_unidas = ', '.join(lista_frases)
 print(frases_unidas)
