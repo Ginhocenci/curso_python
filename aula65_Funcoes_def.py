d2d7ce4bb074692e991a3a66576905d31793c61d
Ginhocenci <63825034+Ginhocenci@users.noreply.github.com>
2025-10-23 13:50:55 -0300

    Create aula65_Funcoes_def.py

---
+++ aula65_Funcoes_def.py
@@ -0,0 +1,21 @@
+"""
+Introdução às funções (def) em Python
+Funções são trechos de código usados para
+replicar determinada ação ao longo do seu código.
+Elas podem receber valores para parâmetros (argumentos)
+e retornar um valor especifico.
+Por padrão, funções Python retornam None (nada).
+"""
+# def imprimir(a, b, c):
+#     print(a, b, c)
+
+# imprimir(1,2,3)
+# imprimir(4,5,6)
+
+def saudacao(nome='Sem Nome'):
+    print(f'Olá, {nome}')
+
+saudacao('Luiz Otávio')
+saudacao('Maria')
+saudacao('Helena')
+saudacao()
