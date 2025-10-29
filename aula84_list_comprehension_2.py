b97b263f6d55d4b478568570589c692006be7da5
Ginhocenci <63825034+Ginhocenci@users.noreply.github.com>
2025-10-29 11:52:17 -0300

    Create aula84_list_comprehension_2.py

---
+++ aula84_list_comprehension_2.py
@@ -0,0 +1,37 @@
+# List comprehension em Python
+# List comprehension é uma forma rápida para criar listas
+# a partir de iteráveis.
+# print(list(range(10)))
+lista = []
+for numero in range(10):
+    lista. append(numero)
+# print(lista)
+
+# lista = [numero for numero in range(10)]
+lista = [
+    numero * 2
+    for numero in range(10)
+]
+# print(lista)
+# print(list(range(10)))
+
+# Mapeamento de dados em list comprehension
+produtos = [
+    {'nome': 'p1', 'preco': 20,},
+    {'nome': 'p2', 'preco': 10,},
+    {'nome': 'p3', 'preco': 30,},
+]
+# novos_produtos = [
+#     {**produto, 'preco': produto['preco']*1.05}
+#     for produto in produtos
+# ]
+novos_produtos = [
+    {**produto, 'preco': produto['preco']*1.05}
+    if produto['preco'] > 20 else {**produto}
+    for produto in produtos
+]
+# novos_produtos = [
+#     produto
+#     for produto in produtos
+# ]
+print(*novos_produtos, sep='\n')
