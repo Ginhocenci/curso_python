3ed8d1332deb8209f20eb4f9874176093872dec1
Ginhocenci <63825034+Ginhocenci@users.noreply.github.com>
2025-10-29 11:01:51 -0300

    Create aula78_set_4.py

---
+++ aula78_set_4.py
@@ -0,0 +1,42 @@
+# Sets - Conjuntos em Python (tipo set)
+# Conjuntos são ensinados na matemática
+# https://brasilescola.uol.com.br/matematica/conjunto.htm
+# Representados graficamente pelo diagrama de Venn
+# Sets em Python são mutáveis, porém aceitam apenas
+# tipos imutáveis como valor interno.
+
+# Criando um set
+# set(iterável) ou {1, 2, 3}
+
+# Sets são eficientes para remover valores duplicados
+# de iteráveis.
+# - Não aceitam valores mutáveis;
+# - Seus valores serão sempre únicos;
+# - não tem índexes;
+# - não garantem ordem;
+# - são iteráveis (for, in, not in)
+
+# Métodos úteis:
+# add, update, clear, discard
+s1 = set()
+s1.add('Luiz')
+s1.add(1)
+s1.update(('Olá Mundo', 1, 2, 3, 4))
+# s1.clear()
+s1.discard('Olá Mundo')
+s1.discard('Luiz')
+print(s1)
+
+
+# Operadores úteis:
+# união | união (union) - Une
+# intersecção & (intersection) - Itens presentes em ambos
+# diferença - Itens presentes apenas no set da esquerda
+# diferença simétrica ^ - Itens que não estão em ambos
+s1 = {1, 2, 3}
+s2 = {2, 3, 4}
+s3 = s1 | s2 # União
+s3 = s1 & s2 # Intersecção
+s3 = s1 - s2 # Diferença
+s3 = s1 ^ s2 # Diferença Simétrica
+print(s3)
