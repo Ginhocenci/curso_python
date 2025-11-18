4a996652e7c4e528b1b4f1dc03f4f99012e588a4
Ginhocenci <63825034+Ginhocenci@users.noreply.github.com>
2025-11-18 15:58:41 -0300

    Create __init__.py

---
+++ aula99_package/__init__.py
@@ -0,0 +1,9 @@
+# print(
+#     'Você importou', __name__
+# )
+
+# def dobra(x):
+#     return x * 2
+# from .modulo import nova_variavel, variavel, soma_do_modulo
+from .modulo import *
+from .modulo_b import *
