a761f3f0271ba6198f50b73e6e344dd0d835aa63
Ginhocenci <63825034+Ginhocenci@users.noreply.github.com>
2025-11-18 15:58:43 -0300

    Create modulo.py

---
+++ aula99_package/modulo.py
@@ -0,0 +1,17 @@
+# __all__ = [
+#     'variavel',
+#     'soma_do_modulo',
+#     'nova_variavel',
+# ]
+# from aula99_package.modulo_b import fala_oi
+#from .modulo_b import fala_oi # Também poderia usar como "."
+
+variavel = 'Alguma coisa'
+
+def soma_do_modulo(x, y):
+    return x + y
+
+
+nova_variavel = 'OK'
+
+# fala_oi()
